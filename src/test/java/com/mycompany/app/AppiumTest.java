113592012cacedfe3b64b50f3920012fa91d646d
unknown <yangcao1307@hotmail.com>
2019-04-13 22:47:55 +1000

    first commit

---
+++ src/test/java/com/mycompany/app/AppiumTest.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
